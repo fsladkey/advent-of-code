3f12499e75a7b51c87b00e27f1209c81060cd122
Fred Sladkey <fsladkey@gmail.com>
2016-12-07 23:10:29 -0500

    Finish day 2

--- 1.py
+++ 1.py
@@ -1,8 +1,7 @@
-data = "R3, R1, R4, L4, R3, R1, R1, L3, L5, L5, L3, R1, R4, L2, L1, R3, L3, R2, R1, R1, L5, L2, L1, R2, L4, R1, L2, L4, R2, R2, L2, L4, L3, R1, R4, R3, L1, R1, L5, R4, L2, R185, L2, R4, R49, L3, L4, R5, R1, R1, L1, L1, R2, L1, L4, R4, R5, R4, L3, L5, R1, R71, L1, R1, R186, L5, L2, R5, R4, R1, L5, L2, R3, R2, R5, R5, R4, R1, R4, R2, L1, R4, L1, L4, L5, L4, R4, R5, R1, L2, L4, L1, L5, L3, L5, R2, L5, R4, L4, R3, R3, R1, R4, L1, L2, R2, L1, R4, R2, R2, R5, R2, R5, L1, R1, L4, R5, R4, R2, R4, L5, R3, R2, R5, R3, L3, L5, L4, L3, L2, L2, R3, R2, L1, L1, L5, R1, L3, R3, R4, R5, L3, L5, R1, L3, L5, L5, L2, R1, L3, L1, L3, R4, L1, R3, L2, L2, R3, R3, R4, R4, R1, L4, R1, L5"
+DIRS = ((0, 1), (1, 0), (0, -1), (-1, -0))
+letter_to_dir = {"R": 1, "L": -1}
 
 def easter_bunny_hq(data):
-    DIRS = ((0, 1), (1, 0), (0, -1), (-1, -0))
-    letter_to_dir = {"R": 1,"L": -1}
     steps = data.split(", ")
     dir_idx = 0
     pos = [0, 0]
@@ -17,7 +16,5 @@ def easter_bunny_hq(data):
             pos[1] += dir[1]
     return abs(pos[0]) + abs(pos[1])
 
-print(easter_bunny_hq("R2, L3"))
-print(easter_bunny_hq("R2, R2, R2"))
-print(easter_bunny_hq("R5, L5, R5, R3"))
+data = open('1_data.txt', 'r').read()
 print(easter_bunny_hq(data))
---
+++ 2.py
@@ -0,0 +1,30 @@
+DIRS = ((0, 1), (1, 0), (0, -1), (-1, -0))
+keypad = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+char_to_dir = {"R": (0, 1), "U": (-1, 0), "D": (1, 0), "L": (0, -1)}
+
+
+def maybe_increment(num, val):
+    newNum = num + val
+    return num if newNum < 0 or newNum > 2 else newNum
+
+
+def bathroom_security(input):
+    steps = input.strip().split("\n")
+    pos = [1, 1]
+    result = []
+    for step in steps:
+        for char in step:
+            dir = char_to_dir[char]
+            for idx in range(2):
+                pos[idx] = maybe_increment(pos[idx], dir[idx])
+        x, y = pos
+        result.append(keypad[x][y])
+    return "".join(list(map(str, result)))
+
+data = open('2_data.txt', 'r').read()
+print(bathroom_security(data))
